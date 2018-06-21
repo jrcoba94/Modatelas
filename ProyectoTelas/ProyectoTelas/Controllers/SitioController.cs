396ea11e14d807a6154b0c6518e76e9d347ecc91
Daryl Segovia Alonzo <daryl_segovia@hotmail.com>
2018-06-20 21:43:28 -0500

    Template para el inicio

---
+++ ProyectoTelas/ProyectoTelas/Controllers/SitioController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoTelas.Controllers
+{
+    public class SitioController : Controller
+    {
+        // GET: Sitio
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+    }
+}
